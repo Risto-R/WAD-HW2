554965fab980526c924580029d0a15eb1361a462
Risto-R <rahulan.risto@gmail.com>
2019-10-10 15:25:03 +0300

    Now adds content dynamically from user and course classes

--- res/js/script.js
+++ res/js/script.js
@@ -1,11 +1,13 @@
 $(function () {
-    var user = new User("John", "Doe", 11 / 10 / 1990, "Software Engineering", 2.75);
+    var user = new User("Joe", "Mama", 10/10/1999, "Faculty of Science and Technology", 3.5);
     var courses = [
-        new Course("Agile software development", 1, 82),
-        new Course("System modeling", 1, 85),
-        new Course("Object-oriented programming", 2, 99),
-        new Course("Estonian language Level A2", 2, 65)
+        new Course("Web Application Development", 3, 100),
+        new Course("Introduction to Data Science", 3, 91),
+        new Course("Software Engineering", 3, 87),
+        new Course("Algorithms and data structures", 3, 85)
     ];
+
+    init();
 
     $("#courses-button").click(function () {
         $("#profile-container").removeClass("active");
@@ -19,9 +21,27 @@ $(function () {
         $("#courses-button").removeClass("active");
         $("#profile-container").addClass("active");
         $("#profile-button").addClass("active");
-    })
+    });
 
+    $("#add-course-button").click(function () {
+        $("#add-course").toggle();
+    });
 
+    function init() {
+        // Add user info to html
+        $("#name").text(user.firstname+" "+user.lastname);
+        $("#birthday").text(user.birthday);
+        $("#faculty").text(user.faculty);
+        $("#gpa strong").text(user.gpa);
 
-
+        // Add rows to table
+        let rows = ($("#courses tr").length);
+        for (let i = 0; i < courses.length; i++) {
+            let index = "<td>"+(rows+i)+"</td>";
+            let title = "<td>"+courses[i].title+"</td>";
+            let semster= "<td>"+courses[i].semester+"</td>";
+            let grade = "<td>"+courses[i].grade+"</td>";
+            $("#courses tr:last").after("<tr>"+index+title+semster+grade+"</tr>")
+        }
+    }
 });
